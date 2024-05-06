20f04adbd86286faf58d5ea1412314de44ec2f38
vitor marini <vitorcmarini@hotmail.com>
2024-05-06 13:57:24 -0300

    resolucao do desafio modelo simplificado

--- src/main/java/br/com/alura/screensound/main/Principal.java
+++ src/main/java/br/com/alura/screensound/main/Principal.java
@@ -1,10 +1,23 @@
 package br.com.alura.screensound.main;
 
+import br.com.alura.screensound.model.Artista;
+import br.com.alura.screensound.model.Musica;
+import br.com.alura.screensound.model.TipoArtista;
+import br.com.alura.screensound.repository.ArtistaRepository;
+import br.com.alura.screensound.service.ConsultaChatGPT;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Principal {
 
+    private final ArtistaRepository repositorio;
     private Scanner leitura = new Scanner(System.in);
+
+    public Principal(ArtistaRepository repositorio) {
+        this.repositorio = repositorio;
+    }
 
     public void exibeMenu() {
         var opcao = -1;
@@ -52,18 +65,55 @@ public class Principal {
     }
 
     private void cadastrarArtistas() {
+        var cadastrarNovo = "S";
+
+        while (cadastrarNovo.equalsIgnoreCase("s")) {
+            System.out.println("Informe o nome desse artista: ");
+            var nome = leitura.nextLine();
+            System.out.println("Informe o tipo desse artista: (solo, dupla ou banda)");
+            var tipo = leitura.nextLine();
+            TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
+            Artista artista = new Artista(nome, tipoArtista);
+            repositorio.save(artista);
+            System.out.println("Cadastrar novo artista? (S/N)");
+            cadastrarNovo = leitura.nextLine();
+        }
     }
 
     private void cadastrarMusicas() {
+        System.out.println("Deseja cadastrar musicas de qual artista? ");
+        var nome = leitura.nextLine();
+        Optional<Artista> artista = repositorio.findByNomeContainingIgnoreCase(nome);
+        if (artista.isPresent()) {
+            System.out.println("Informe o titulo da música: ");
+            var nomeMusica = leitura.nextLine();
+            Musica musica = new Musica(nomeMusica);
+            musica.setArtista(artista.get());
+            artista.get().getMusica().add(musica);
+            repositorio.save(artista.get());
+        } else {
+            System.out.println("Artista não encontrado!");
+        }
     }
 
     private void listarMusicas() {
+        List<Artista> artistas  = repositorio.findAll();
+        artistas.forEach(a -> a.getMusica().forEach(System.out::println));
     }
 
     private void buscarMusicasPorArtista() {
+        System.out.println("Deseja Buscar musicas de qual artista? ");
+        var nome = leitura.nextLine();
+        List<Musica> musicas = repositorio.buscaMusicasPorArtista(nome);
+        musicas.forEach(System.out::println);
+
     }
 
     private void pesquisarDadosDoArtista() {
+        System.out.println("Pesquisar dados de qual artista: ");
+        var nome = leitura.nextLine();
+        var resposta = ConsultaChatGPT.obterInformacao(nome);
+        System.out.println(resposta.trim());
     }
 
 }
